eead77d49f78594238161d4311ae973fc0b15b5e
Tawanchai+ <gastonferrarisdavies@gmail.com>
2025-02-02 21:36:41 -0300

    chama

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,7 @@
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Home from './pages/Home';
 import Products from './pages/Products';
-<<<<<<< HEAD
-<<<<<<< HEAD
-import Admin from './pages/Admin';
-=======
 import Prueba from './pages/Prueba';
->>>>>>> 421941c2b4219465d2d69ed6a84cfdf91dcb92fb
-=======
-import Prueba from './pages/Prueba';
->>>>>>> 421941c2b4219465d2d69ed6a84cfdf91dcb92fb
 import './App.css'
 
 export function App() {
@@ -21,15 +13,7 @@ export function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/products" element={<Products />} />
-<<<<<<< HEAD
-<<<<<<< HEAD
-        <Route path="/admin" element={<Admin />} />
-=======
         <Route path="/prueba" element={<Prueba />} />
->>>>>>> 421941c2b4219465d2d69ed6a84cfdf91dcb92fb
-=======
-        <Route path="/prueba" element={<Prueba />} />
->>>>>>> 421941c2b4219465d2d69ed6a84cfdf91dcb92fb
       </Routes>
     </Router>
     </>
